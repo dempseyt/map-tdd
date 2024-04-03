24b5572c1ed0af48ac1b70cde49bae2cc923b89f
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-03 15:19:31 +0800

    RED: map maps over objects

--- src/map.test.js
+++ src/map.test.js
@@ -7,5 +7,9 @@ describe("map", () => {
 
     it('maps simple functions over arrays', () => {
         expect(map(times2, [1,2,3,4])).toStrictEqual([2,4,6,8]);
-    })
+    });
+    it('maps over objects', () => {
+        expect(map(dec({}))).toStrictEqual({});
+        expect(map(dec({x: 4, y: 5, z: 6}))).toStrictEqual({x: 3, y: 4, z: 5});
+    });
 });
