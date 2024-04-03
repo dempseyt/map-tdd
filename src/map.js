6face382dd798c3fea434cad679faeaab348133d
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-03 15:17:07 +0800

    GREEN: map maps simple functions over arrays

--- src/map.js
+++ src/map.js
@@ -1,5 +1,9 @@
-function map() {
-
+function map(mapper, iterable) {
+    const mapped = [];
+    for (const item of iterable) {
+        mapped.push(mapper(item))
+    }
+    return mapped;
 }
 
 export default map;
