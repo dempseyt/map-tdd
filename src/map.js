0858c67addc8b22d891971fe843761ca3018d71c
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-05 13:47:16 +0800

    GREEN: map maps over objects

--- src/map.js
+++ src/map.js
@@ -1,9 +1,31 @@
-function map(mapper, iterable) {
-    const mapped = [];
-    for (const item of iterable) {
-        mapped.push(mapper(item))
-    }
-    return mapped;
+function getIsPlainObject(value) {
+    return (
+        value !== null &&
+        typeof value === 'object' &&
+        Object.getPrototypeOf(value) === Object.prototype
+    );
+}
+
+function map(func, functor) {
+    let mappedFunctor;
+    const isPlainObject = getIsPlainObject(functor);
+    console.log(func)
+    
+    if (isPlainObject) {
+        mappedFunctor = {};
+
+        for (const [key, value] of Object.entries(functor)) {
+            mappedFunctor[key] = func(value);
+        }
+    } 
+    else if (Array.isArray(functor)) {
+        mappedFunctor = [];
+
+        for (let i = 0; i < functor.length; i++) {
+            mappedFunctor[i] = func(functor[i]);
+        }
+    }  
+    return mappedFunctor;
 }
 
 export default map;
--- src/map.test.js
+++ src/map.test.js
@@ -9,7 +9,7 @@ describe("map", () => {
         expect(map(times2, [1,2,3,4])).toStrictEqual([2,4,6,8]);
     });
     it('maps over objects', () => {
-        expect(map(dec({}))).toStrictEqual({});
-        expect(map(dec({x: 4, y: 5, z: 6}))).toStrictEqual({x: 3, y: 4, z: 5});
+        expect(map(dec, {})).toStrictEqual({});
+        expect(map(dec, {x: 4, y: 5, z: 6})).toStrictEqual({x: 3, y: 4, z: 5});
     });
 });
