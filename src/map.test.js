3bb0fefb5cbc9ffd7cabea289903dbca44730a08
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-17 14:18:15 +0800

    RED/GREEN: map can act as a transducer

--- src/map.test.js
+++ src/map.test.js
@@ -1,4 +1,5 @@
 import map from './map';
+import * as R from 'ramda';
 
 describe("map", () => {
     const times2 = (x) => x*2;
@@ -48,5 +49,9 @@ describe("map", () => {
 
         expect(xcomp.xf).toEqual({xf: listXf, f: times2});
         expect(xcomp.f).toEqual(dec);
+    });
+    it('can act as a transducer', () => {
+        expect(R.into([], R.map(times2), [1,2,3,4])).toEqual([2,4,6,8]);
+        expect(R.transduce(R.map(times2), R.flip(R.append), [], [1,2,3,4])).toEqual([2,4,6,8])
     })
 });
